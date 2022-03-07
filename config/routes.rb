3bd90fd82f5e12644b8da619ca76b88992ce134a
braum852 <julian.teo59@yahoo.com>
2022-03-07 19:14:54 +1000

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
+  get 'users/new', to: 'users#new', as: 'register'
 end
+#$ rails routes
+#posts   GET  /posts(.:format)       posts#index
+#post    GET  /posts/:id(.:format)   posts#show
